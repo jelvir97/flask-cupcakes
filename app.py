74ac995257a07f7a2c9442b281d6b97881fe3fd1
Juan Elvir <jelvir97@gmail.com>
2023-07-27 11:57:37 -0600

    functionality for adding new cupcakes from front end

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'lowlypotato'
 
-debug = DebugToolbarExtension(app)
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# debug = DebugToolbarExtension(app)
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 app.app_context().push()
 connect_db(app)
--- static/app.js
+++ static/app.js
@@ -1,7 +1,14 @@
 
 const $container = $('#cupcake-container')
+const $form = $('#cupcake-form')
+const $addBtn = $('#add-button')
+const $cancel = $('#cancel-add')
+const $submit = $('#submit')
 
 $(loadAllCupcakes);
+$addBtn.on('click',handleAddBtnClick)
+$cancel.on('click', handleCancelClick)
+$submit.on('click', handleAddCupcakeSubmit)
 
 async function loadAllCupcakes(){
     response = await axios.get('/api/cupcakes')
@@ -9,7 +16,7 @@ async function loadAllCupcakes(){
     cupcakes = response.data.cupcakes
     console.log(cupcakes)
     for(let cupcake of cupcakes){
-        const $cupcake = $(`<img src="${cupcake.image}">
+        const $cupcake = $(`<img src="${cupcake.image}" style="max-height: 150px;">
                             <h3 data-id="${cupcake.id}">${cupcake.flavor}</h3>
                             <ul>
                                 <li>Size:${cupcake.size}</li>
@@ -17,4 +24,53 @@ async function loadAllCupcakes(){
                             </ul>`)
         $container.append($cupcake)
     }
+}
+
+function handleAddBtnClick(){
+    $form.show()
+    $addBtn.hide()
+}
+
+function handleCancelClick(e){
+    e.preventDefault()
+    $form.hide()
+    $addBtn.show()
+}
+
+// form input variables
+const $image = $('#image')
+const $flavor = $('#flavor')
+const $size = $('#size')
+const $rating = $('#rating')
+
+async function handleAddCupcakeSubmit(e){
+    e.preventDefault()
+    if (!validateFormInputs()) return
+    console.log('Passed Validation')
+    jsonResp = getJSON();
+    await axios.post('/api/cupcakes', jsonResp, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    $container.empty()
+    loadAllCupcakes()
+}
+
+function validateFormInputs(){
+    console.log('in validation')
+    if (!$flavor.val()) return false
+    if (!$size.val()) return false
+    if (!$rating.val()) return false
+    return true
+}
+function getJSON(){
+    obj ={
+        flavor: $flavor.val(),
+        size: $size.val(),
+        rating: $rating.val(),
+        image: $image.val()
+    }
+
+    return JSON.stringify(obj);
 }
